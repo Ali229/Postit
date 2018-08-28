d5a8db7c6262ea69154922410981a28efd1d6ac0
Muhammad Ali <alinaeem229@hotmail.com>
2018-08-28 15:29:39 -0400

    Initialize sigup

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,27 +1,8 @@
 import React, {Component} from 'react';
-import {firebaseApp} from '../firebase'
 
 class SignUp extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      email: '',
-      password: ''
-    }
-  }
-
-  signUp() {
-    console.log('this.state', this.state);
-    const {email, password} = this.state;
-    firebaseApp.auth().createUserWithEmailAndPassword(email, password).catch(error => {
-      console.log('error', error)
-    })
-  }
-
   render() {
-
     return (<h1>Sign up</h1>)
   }
 }
-
 export default SignUp;
