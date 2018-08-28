b30f6e0f6291258cd5e6dc7af16c576da9694cd3
Muhammad Ali <alinaeem229@hotmail.com>
2018-08-28 15:31:05 -0400

    Removed firebase

--- src/index.js
+++ src/index.js
@@ -1,18 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 import {Switch, Route} from 'react-router-dom';
-import {firebaseApp} from './firebase'
 import HashRouter from "react-router-dom/es/HashRouter"
 import SignIn from './components/SignIn';
 import SignUp from './components/SignUp';
-firebaseApp.auth().onAuthStateChanged(user => {
-  if (user) {
-    console.log('User has signed in or up', user);
-  } else {
-    console.log('user has signed out');
-  }
-});
 
 ReactDOM.render(<HashRouter basename={"/ledger"}>
   <Switch>
