93b3a137f861fcf77fe7208a603f3710d0341a16
Muhammad Ali <alinaeem229@hotmail.com>
2018-09-10 21:06:01 -0400

    Fetch Update

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,25 +1,40 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom'
-
 class SignIn extends Component {
-  handleClick() {
-    fetch('http://alinaeem.mynetgear.com:9090/ledger/', {
-      method: 'POST',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({username: 'inputEmail'})
-    }).then(response => response.json()).catch(error => console.log('fetch error: ', error))
+
+  update() {
+    console.log('running...');
+    const userObject = {
+      email: document.getElementById("inputEmail").value || null,
+      password: document.getElementById("inputPassword").value || null,
+      name: "jacob"
+    };
+
+    putData('http://markzeagler.com/ledger-backend/signin', userObject)
+      .then((res) => console.log("This is after function call", JSON.stringify(res)))
+      .catch((err) => console.log("If err", err))
+
+    function putData(url, data) {
+      console.log("Makind sure data is passed", data);
+      return fetch(url,  {
+        method: "PUT",
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(userObject)
+      }).then(response => response.json());
+    }
   }
+
   render() {
     document.body.classList.add("gradient");
     return (<form className="form-signin">
       <img className="i1" src="logo.png" alt="logo"/>
       <h1>LOGIN</h1>
-      <input type="email" id="inputEmail" className="form-control" placeholder="Email" required="" autoFocus=""/>
+      <input type="email" id="inputEmail" className="form-control" placeholder="Email" required=""/>
       <input type="password" id="inputPassword" className="form-control" placeholder="Password" required=""/>
-      <button className="sb btn btn-lg btn-primary btn-block" type="submit" onClick={this.handleClick}>LOGIN</button>
+      <button className="sb btn btn-lg btn-primary btn-block" type="button" onClick={this.update}>LOGIN</button>
       <p className="links" align="left" margin-top="10px">&#x25C8;&nbsp;
         <Link to="/forgot">Forgot password?</Link><br/>
         &#x25C8;&nbsp;
