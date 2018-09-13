4125e15cbb56c56e73132b31d5bf0636f32d4beb
Muhammad Ali <alinaeem229@hotmail.com>
2018-09-13 18:17:56 -0400

    Removed Basename

--- src/components/ForgotPass.jsx
+++ src/components/ForgotPass.jsx
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 
 class ForgotPass extends Component {
   render() {
+    document.body.classList.remove("gradient");
     return (<h1>Forgot Password</h1>)
   }
 }
