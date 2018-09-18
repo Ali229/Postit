ef1a2fc57dc7abc264dc866bdff18eaa227866d5
Muhammad Ali <alinaeem229@hotmail.com>
2018-09-18 18:04:59 -0400

    Redirect 9.0

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter, Route, Switch /*,Redirect*/} from 'react-router-dom'
+import {
+  BrowserRouter, Route, Switch, Redirect
+} from 'react-router-dom'
 import Home from './components/Home';
 import SignIn from './components/SignIn';
 import SignUp from './components/SignUp';
@@ -9,12 +11,11 @@ import ForgotPass from './components/ForgotPass';
 //    <Route path='/*' component={SignIn}/>
 ReactDOM.render((<BrowserRouter basename="/ledger">
   <Switch>
-    <Redirect from='/' to='/signin'/>
     <Route path='/home' component={Home}/>
     <Route path='/signup' component={SignUp}/>
     <Route path='/forgot' component={ForgotPass}/>
     <Route path='/signin' component={SignIn}/>
     <Route path='/' component={SignIn}/>
-
+    <Redirect from='/' to='/signin'/>
   </Switch>
 </BrowserRouter>), document.getElementById('root'));
