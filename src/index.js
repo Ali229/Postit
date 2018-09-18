c5ec52218562e6fd0d76c19ba399750bc6d172ff
Muhammad Ali <alinaeem229@hotmail.com>
2018-09-18 17:14:09 -0400

    Redirect 5.0

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter, Route, Switch} from 'react-router-dom'
+import {BrowserRouter, Route, Switch, Redirect} from 'react-router-dom'
 import Home from './components/Home';
 import SignIn from './components/SignIn';
 import SignUp from './components/SignUp';
@@ -8,10 +8,11 @@ import ForgotPass from './components/ForgotPass';
 
 ReactDOM.render((<BrowserRouter basename="/ledger">
   <Switch>
-  <Route path="/home" component={Home}/>
-    <Route path="/signup" component={SignUp}/>
-    <Route path="/forgot" component={ForgotPass}/>
-    <Route path="/signin" component={SignIn}/>
-    <Route path="/" component={SignIn}/>
+    <Redirect from='/' to='/signin'>
+    <Route path='/home' component={Home}/>
+    <Route path='/signup' component={SignUp}/>
+    <Route path='/forgot' component={ForgotPass}/>
+    <Route path='/signin' component={SignIn}/>
+    <Route path='/' component={SignIn}/>
   </Switch>
 </BrowserRouter>), document.getElementById('root'));
