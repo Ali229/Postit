a0871bca3012d6df86b8a0cfb0f550bf169b99da
Muhammad Ali <alinaeem229@hotmail.com>
2018-09-18 21:50:11 -0400

    cleanup

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -4,45 +4,38 @@ import {Link} from 'react-router-dom';
 class SignIn extends Component {
 
   login() {
-  const userObject = {
-    username: document.getElementById('inputUsername').value || null,
-    password: document.getElementById('inputPassword').value || null
-  };
-  console.log('User object is: ', userObject);
-  if (!userObject.username || !userObject.password) {
-    return console.log('Username, password empty!');
-  }
-    putData('http://markzeagler.com/ledger-backend/signin', userObject)
-    .then((res) => {
-      console.log('Res: ', res);
-      const result = {
-        //message: res.message,
-        status: res.status_code
-      };
+    const userObject = {
+      username: document.getElementById('inputUsername').value || null,
+      password: document.getElementById('inputPassword').value || null
+    };
+    if (!userObject.username || !userObject.password) {
+      return console.log('Username, password empty!');
+    }
 
-      console.log('Result status first: ', result.status);
-      if (!result.status || result.status !== 200) {
-        return console.log('Result status: ', result.status);
-      }
-      return window.location = '/ledger/home';
-      })
+    putData('http://markzeagler.com/ledger-backend/signin', userObject)
+    // .then((data) => {
+    //
+    //   console.log(JSON.stringify(data));
+    //
+    //   if (!result.status || result.status !== 200) {
+    //     return console.log('Result status: ', result.status);
+    //   }
+    //   return window.location = '/ledger/home';
+    //   })
+    .then((data) => console.log(JSON.stringify(data)))
     .catch((err) => console.log('Caught Error: ', err))
 
-function putData(url, data) {
-  console.log('Data Passed: ', data);
-  console.log(JSON.stringify(data));
-  console.log(url);
-  return fetch(url, {
-    method: 'PUT',
-    body: JSON.stringify(data),
-    headers: {
-      'Content-Type': 'application/json'
+    function putData(url, data) {
+      return fetch(url, {
+        method: 'PUT',
+        cache: 'no-cache',
+        body: JSON.stringify(data),
+        referrer: 'no-referrer',
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      }).then(response => response.json())
     }
-  }).then(response => console.log(response))
-  .catch((error) => {
-    console.log('Fetching Error: ', error);
-  });
-}
   }
 
 
