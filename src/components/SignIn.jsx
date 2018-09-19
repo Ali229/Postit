e81e500d659a75055888509a7681b08141d419f8
Muhammad Ali <alinaeem229@hotmail.com>
2018-09-19 13:31:41 -0400

    fix 3.8

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -34,7 +34,9 @@ class SignIn extends Component {
       },
       body: JSON.stringify(data)
     })
-    .then(response => return response.json());
+    .then((response) => {
+      return response.json()
+    });
   }
 
 
