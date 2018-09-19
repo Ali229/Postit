cafd39c70eb643c958de735e0031a51339605a4d
Muhammad Ali <alinaeem229@hotmail.com>
2018-09-18 22:23:33 -0400

    Reverting

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -5,47 +5,45 @@ class SignIn extends Component {
 
   login() {
     const userObject = {
-      username: document.getElementById('inputUsername').value || null,
-      password: document.getElementById('inputPassword').value || null
+      username: document.getElementById("inputEmail").value || null,
+      password: document.getElementById("inputPassword").value || null,
     };
-    if (!userObject.username || !userObject.password) {
-      return console.log('Username, password empty!');
-    }
-
     putData('http://markzeagler.com/ledger-backend/signin', userObject)
-    // .then((data) => {
-    //
-    //   console.log(JSON.stringify(data));
-    //
-    //   if (!result.status || result.status !== 200) {
-    //     return console.log('Result status: ', result.status);
-    //   }
-    //   return window.location = '/ledger/home';
-    //   })
-    .then((data) => console.log(JSON.stringify(data)))
-    .catch((err) => console.log('Caught Error: ', err))
+    .then((res) => {
+      const result = {
+        message: res.message,
+        status: res.status_code
+      };
+      console.log("result is ", result);
+      if (!result.status || result.status !== 200) {
+        console.log('server threw an error');
+      }
+    return window.location = "/ledger/home";
+    })
+    .catch((err) => console.log("If err", err))
 
     function putData(url, data) {
-      return fetch(url, {
-        method: 'PUT',
-        cache: 'no-cache',
-        body: JSON.stringify(data),
-        referrer: 'no-referrer',
+      console.log("Makind sure data is passed", data);
+      return fetch(url,  {
+        method: "PUT",
         headers: {
+          'Accept': 'application/json',
           'Content-Type': 'application/json'
-        }
-      }).then(response => response.json())
+        },
+        body: JSON.stringify(userObject)
+      }).then(response => response.json());
     }
   }
 
 
   render() {
-  document.body.classList.add('gradient');
-  return (<div className="main-border">
-    <form className="form-signin">
-      <img className="i1" src="signin_logo.png" alt="logo"/>
+    document.body.classList.add("gradient");
+    return (
+      <div className="main-border">
+      <form className="form-signin">
+      <img className="i1" src="logo.png" alt="logo"/>
       <h1>LOGIN</h1>
-      <input type="text" id="inputUsername" className="form-control" placeholder="Username" required=""/>
+      <input type="email" id="inputEmail" className="form-control" placeholder="Email" required=""/>
       <input type="password" id="inputPassword" className="form-control" placeholder="Password" required=""/>
       <button className="sb btn btn-lg btn-primary btn-block" type="submit" onClick={this.login}>LOGIN</button>
       <p className="links" align="left" margin-top="10px">&#x25C8;&nbsp;
@@ -55,8 +53,9 @@ class SignIn extends Component {
       </p>
       <p className="mt-5 mb-3 text-muted" align="left">© 2018</p>
     </form>
-  </div>)
-}
+    </div>
+  )
+  }
 }
 
 export default SignIn;
