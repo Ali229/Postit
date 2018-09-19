6c6816209dcbba00d19afc7da2e6be9b2aebf678
Muhammad Ali <alinaeem229@hotmail.com>
2018-09-19 13:24:19 -0400

    3.7

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -2,11 +2,11 @@ import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
 
 class SignIn extends Component {
-  //
-  // constructor() {
-  //   super();
-  //   this.login = this.login.bind(this);
-  // }
+  
+  constructor() {
+    super();
+    this.login = this.login.bind(this);
+  }
 
   login() {
     const userObject = {
