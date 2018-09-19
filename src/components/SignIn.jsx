1df7b2afa98714b8ee936f9fd827b3df95f3e313
Muhammad Ali <alinaeem229@hotmail.com>
2018-09-19 12:54:30 -0400

    3.6

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -13,12 +13,10 @@ class SignIn extends Component {
     }
     this.putData('http://markzeagler.com/ledger-backend/signin', userObject)
       .then((res) => {
-        console.log(res);
-        const result = {
-          message: JSON.stringify(res)
-        };
-        console.log('Success: ', result.message);
-        return window.location = '/ledger/home';
+        const result = JSON.stringify(res);
+        console.log(result);
+        // console.log('Success: ', result.message);
+        // return window.location = '/ledger/home';
       })
       .catch((err) => console.log('Caught Error: ', err));
   }
@@ -32,11 +30,7 @@ class SignIn extends Component {
       },
       body: JSON.stringify(data)
     })
-      .then(response => {
-        const responseObject = {
-          status: response.status,
-        };
-        console.log(responseObject);
+      .then((response) => {
         return responseObject.json();
       })
       // .catch(err => console.log('Error from backend ', err));
