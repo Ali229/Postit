dea82c762f90ce78365fc20c9ee6d672cbd05cf3
Muhammad Ali <alinaeem229@hotmail.com>
2018-09-20 17:46:32 -0400

    1.2

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -16,51 +16,51 @@ class SignIn extends Component {
     if (!userObject.username || !userObject.password) {
       return console.error('Username or password is empty');
     }
-    connect('http://markzeagler.com/ledger-backend/signin', userObject);
+    this.connect('http://markzeagler.com/ledger-backend/signin', userObject);
   }
   async connect(url, userObject) {
     try {
-      let response = await fetch(url), {
-          method: "PUT",
-          headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify(userObject)
-        });
-        let resJSON = await response.json();
-        console.info(JSON.stringify(resJSON));
-      } catch (err) {
-        console.error('Error: ', err);
-      }
-    }
-
-    // state = { showError: true }
-    //
-    //  toggleError = () => {
-    //    this.setState((prevState, props) => {
-    //      return { showError: !prevState.showError }
-    //    })
-    //  };
-
-    render() {
-      document.body.classList.add('gradient');
-      return (<div className="main-border">
-        <form className="form-signin">
-          <img className="i1" src="signin_logo.png" alt="logo"/>
-          <h1>LOGIN</h1>
-          <input type="email" id="inputUsername" className="form-control" placeholder="Username" required=""/>
-          <input type="password" id="inputPassword" className="form-control" placeholder="Password" required=""/>
-          <button className="sb btn btn-lg btn-primary btn-block" type="submit" onClick={this.login}>LOGIN</button>
-          <p className="links" align="left" margin-top="10px">&#x25C8;&nbsp;
-            <Link to="/forgot">Forgot password?</Link><br/>
-            &#x25C8;&nbsp;
-            <Link to="/signup">Create account</Link>
-          </p>
-          <p className="mt-5 mb-3 text-muted" align="left">© 2018</p>
-        </form>
-      </div>)
+      let response = await fetch((url), {
+        method: "PUT",
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(userObject)
+      });
+      let resJSON = await response.json();
+      console.info(JSON.stringify(resJSON));
+    } catch (err) {
+      console.error('Error: ', err);
     }
   }
 
-  export default SignIn;
+  // state = { showError: true }
+  //
+  //  toggleError = () => {
+  //    this.setState((prevState, props) => {
+  //      return { showError: !prevState.showError }
+  //    })
+  //  };
+
+  render() {
+    document.body.classList.add('gradient');
+    return (<div className="main-border">
+      <form className="form-signin">
+        <img className="i1" src="signin_logo.png" alt="logo"/>
+        <h1>LOGIN</h1>
+        <input type="email" id="inputUsername" className="form-control" placeholder="Username" required=""/>
+        <input type="password" id="inputPassword" className="form-control" placeholder="Password" required=""/>
+        <button className="sb btn btn-lg btn-primary btn-block" type="submit" onClick={this.login}>LOGIN</button>
+        <p className="links" align="left" margin-top="10px">&#x25C8;&nbsp;
+          <Link to="/forgot">Forgot password?</Link><br/>
+          &#x25C8;&nbsp;
+          <Link to="/signup">Create account</Link>
+        </p>
+        <p className="mt-5 mb-3 text-muted" align="left">© 2018</p>
+      </form>
+    </div>)
+  }
+}
+
+export default SignIn;
