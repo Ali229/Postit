4c0eae5c872fb38dfd385bf9f51af3e42cf0f231
Muhammad Ali <alinaeem229@hotmail.com>
2018-09-20 17:32:36 -0400

    Loggin in using Async Test 1.0

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -5,80 +5,62 @@ class SignIn extends Component {
 
   constructor() {
     super();
-      this.login = this.login.bind(this);
+    this.login = this.login.bind(this);
   }
 
   login() {
     const userObject = {
       username: document.getElementById('inputUsername').value || null,
-      password: document.getElementById('inputPassword').value || null,
+      password: document.getElementById('inputPassword').value || null
     };
     if (!userObject.username || !userObject.password) {
-      return console.log('Incorrect username and password');
+      return console.error('Username or password is empty');
+    }
+    connect('http://markzeagler.com/ledger-backend/signin', userObject);
+  }
+  async connect(url, userObject) {
+    try {
+      let response = await fetch(url), {
+          method: "PUT",
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+          }
+          body: JSON.stringify(userObject)
+        });
+        let resJSON = await response.json();
+        console.info(JSON.stringify(resJSON));
+      } catch (err) {
+        console.error('Error: ', err);
+      }
     }
 
-    this.putData('http://markzeagler.com/ledger-backend/signin', userObject)
-      .then((res) => {
-        const result = JSON.stringify(res);
+    // state = { showError: true }
+    //
+    //  toggleError = () => {
+    //    this.setState((prevState, props) => {
+    //      return { showError: !prevState.showError }
+    //    })
+    //  };
 
-        // if result.message blalala
-
-        console.log('Result', result);
-      })
-      .catch((err) => {
-        console.log('Error: ', err);
-      })
+    render() {
+      document.body.classList.add('gradient');
+      return (<div className="main-border">
+        <form className="form-signin">
+          <img className="i1" src="signin_logo.png" alt="logo"/>
+          <h1>LOGIN</h1>
+          <input type="email" id="inputUsername" className="form-control" placeholder="Username" required=""/>
+          <input type="password" id="inputPassword" className="form-control" placeholder="Password" required=""/>
+          <button className="sb btn btn-lg btn-primary btn-block" type="submit" onClick={this.login}>LOGIN</button>
+          <p className="links" align="left" margin-top="10px">&#x25C8;&nbsp;
+            <Link to="/forgot">Forgot password?</Link><br/>
+            &#x25C8;&nbsp;
+            <Link to="/signup">Create account</Link>
+          </p>
+          <p className="mt-5 mb-3 text-muted" align="left">© 2018</p>
+        </form>
+      </div>)
+    }
   }
 
-  putData(url, data) {
-    return fetch(url, {
-      method: 'PUT',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(data)
-    })
-    .then(response => {
-      if (!response || !response.status === 200) {
-        throw new Error('Error: ', response.status);
-      }
-      return window.location = "/ledger/home";
-    })
-    .catch((err) => {
-      console.log('err here: ', err);
-      return Promise.reject(err);
-    })
-  }
-
-  state = { showError: true }
-
-   toggleError = () => {
-     this.setState((prevState, props) => {
-       return { showError: !prevState.showError }
-     })
-   };
-
-  render() {
-    document.body.classList.add('gradient');
-    return (
-      <div className="main-border">
-      <form className="form-signin">
-      <img className="i1" src="signin_logo.png" alt="logo"/>
-      <h1>LOGIN</h1>
-      <input type="email" id="inputUsername" className="form-control" placeholder="Username" required=""/>
-      <input type="password" id="inputPassword" className="form-control" placeholder="Password" required=""/>
-      <button className="sb btn btn-lg btn-primary btn-block" type="submit" onClick={this.login}>LOGIN</button>
-      <p className="links" align="left" margin-top="10px">&#x25C8;&nbsp;
-        <Link to="/forgot">Forgot password?</Link><br/>
-        &#x25C8;&nbsp;
-        <Link to="/signup">Create account</Link>
-      </p>
-      <p className="mt-5 mb-3 text-muted" align="left">© 2018</p>
-    </form>
-    </div>
-  )
-  }
-}
-
-export default SignIn;
+  export default SignIn;
