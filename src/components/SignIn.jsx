6b994bd9a388c793d8cb70da09f3be09175b3e6a
Muhammad Ali <alinaeem229@hotmail.com>
2018-09-20 22:54:23 -0400

    Finalizing login

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -13,10 +13,6 @@ class SignIn extends Component {
       username: document.getElementById('inputUsername').value || null,
       password: document.getElementById('inputPassword').value || null
     };
-    if (!userObject.username || !userObject.password) {
-      document.getElementById("errorDiv").style.display = 'block';
-      return document.getElementById("errorDiv").innerHTML = 'Username and Password cannot be empty!';
-    }
     this.connect('http://markzeagler.com/ledger-backend/signin', userObject);
   }
 
@@ -35,10 +31,11 @@ class SignIn extends Component {
       }
       let resJSON = await response.json();
       console.info(JSON.stringify(resJSON));
+      window.location = "/ledger/home";
     } catch (e) {
       console.error(e);
       document.getElementById("errorDiv").style.display = 'block';
-      document.getElementById("errorDiv").innerHTML = e;
+      document.getElementById("errorDiv").innerHTML = "Invalid login details!";
     }
   }
 
@@ -51,7 +48,7 @@ class SignIn extends Component {
         <input type="email" id="inputUsername" className="form-control" placeholder="Username" required=""/>
         <input type="password" id="inputPassword" className="form-control" placeholder="Password" required=""/>
         <div id="errorDiv"></div>
-        <button className="sb btn btn-lg btn-primary btn-block" type="submit" onClick={this.login}>LOGIN</button>
+        <button className="sb btn btn-lg btn-primary btn-block" type="button" onClick={this.login}>LOGIN</button>
         <p className="links" align="left" margin-top="10px">&#x25C8;&nbsp;
           <Link to="/forgot">Forgot password?</Link><br/>
           &#x25C8;&nbsp;
