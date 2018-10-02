745f0dea7862f10b495611e77b8650ab02f4a968
ali229 <alinaeem229@gmail.com>
2018-10-02 16:37:14 -0400

    Login UI

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -5,7 +5,10 @@ import { first } from 'rxjs/operators';
 
 import { AuthenticationService } from '../_services';
 
-@Component({templateUrl: 'login.component.html'})
+@Component({
+    templateUrl: 'login.component.html',
+    styleUrls: ['./style.css']
+})
 export class LoginComponent implements OnInit {
     loginForm: FormGroup;
     loading = false;
