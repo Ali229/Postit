a04b99391960e5bd11bebc95ccc6ddd0f29b0031
Mark Zeagler <mark.zeagler@outlook.com>
2018-10-04 17:51:49 -0400

    Shortened import

--- src/app/_services/authentication.service.ts
+++ src/app/_services/authentication.service.ts
@@ -2,7 +2,7 @@ import {Injectable} from '@angular/core';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {Observable} from "rxjs";
 import {first} from "rxjs/operators";
-import {LoginData} from "../_models/login_data";
+import {LoginData} from "../_models";
 
 @Injectable({
     providedIn: 'root'
