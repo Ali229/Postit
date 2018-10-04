1d0dca834ad21356cb45c16f1f82bf6a5331886a
ali229 <alinaeem229@gmail.com>
2018-10-04 19:27:01 -0400

    Bootstrap mdb working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,22 @@
-﻿import { NgModule }      from '@angular/core';
+﻿import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { ReactiveFormsModule }    from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 // used to create fake backend
 import { fakeBackendProvider } from './_helpers';
-
-import { AppComponent }  from './app.component';
-import { routing }        from './app.routing';
-
+import { AppComponent } from './app.component';
+import { routing } from './app.routing';
 import { BasicAuthInterceptor, ErrorInterceptor } from './_helpers';
 import { HomeComponent } from './home';
 import { LoginComponent } from './login';
-import {AuthenticationService, UserService} from "./_services";;
-import { NavbarComponent } from './navbar/navbar.component'
-;
-import { AccountComponent } from './account/account.component'
-
+import {AuthenticationService, UserService} from './_services';
+import { NavbarComponent } from './navbar/navbar.component';
+import { AccountComponent } from './account/account.component';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
 @NgModule({
     imports: [
+        MDBBootstrapModule.forRoot(),
         BrowserModule,
         ReactiveFormsModule,
         HttpClientModule,
